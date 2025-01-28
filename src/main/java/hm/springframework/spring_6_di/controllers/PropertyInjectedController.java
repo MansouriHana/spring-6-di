a6c6760454d8d9634e016c190abad1774be30501
DESKTOP-Q31DD3F\LENOVO <mansouriii.hanaa@gmail.com>
2025-01-28 11:21:36 +0000

    using-qualifers

--- src/main/java/hm/springframework/spring_6_di/controllers/PropertyInjectedController.java
+++ src/main/java/hm/springframework/spring_6_di/controllers/PropertyInjectedController.java
@@ -2,11 +2,13 @@ package hm.springframework.spring_6_di.controllers;
 
 import hm.springframework.spring_6_di.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class PropertyInjectedController {
 
+    @Qualifier("propertyGreetingService")
     @Autowired
     GreetingService greetingService;
 
--- src/test/java/hm/springframework/spring_6_di/controllers/ConstructorInjectedControllerTest.java
+++ src/test/java/hm/springframework/spring_6_di/controllers/ConstructorInjectedControllerTest.java
@@ -1,12 +1,10 @@
 package hm.springframework.spring_6_di.controllers;
 
-import hm.springframework.spring_6_di.services.GreetingServiceImpl;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class ConstructorInjectedControllerTest {
 
